209a5e631610867c7df5da87662779705388b294
Alexander <pchp250g8@tutamail.com>
2026-08-31 20:24:09 +0300

    Fixed Logical Errors

--- Task-1-1-en/Task-1-1-en.php
+++ Task-1-1-en/Task-1-1-en.php
@@ -5,6 +5,6 @@
     $k = intval(readline());
     $t = intdiv($n, $k); // Maximum number of towers
     $b = $t * $k;  // Number of blocks
-    printf("From %d blocks, you can build a maximum of %d towers\r\n", $b, $t);
+    printf("From %d blocks, you can build a maximum of %d towers\r\n", $n, $t);
     printf("This will take %d blocks.\r\n", $b);
 ?>
