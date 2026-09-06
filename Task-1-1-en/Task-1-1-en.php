4216fc59f916dc590365a13860d8e14158c96f43
Alexander <pchp250g8@tutamail.com>
2026-09-06 14:16:41 +0300

    Fixed Comments

--- Task-1-1-en/Task-1-1-en.php
+++ Task-1-1-en/Task-1-1-en.php
@@ -1,10 +1,12 @@
 <?php
+    /*Input data*/
     print("Enter the number of blocks: ");
     $n = intval(readline());
     print("Enter the number of blocks for 1 tower: ");
     $k = intval(readline());
     $t = intdiv($n, $k); // Maximum number of towers
     $b = $t * $k;  // Number of blocks
+    /*Output information*/
     printf("From %d blocks, you can build a maximum of %d towers\r\n", $n, $t);
     printf("This will take %d blocks.\r\n", $b);
 ?>
